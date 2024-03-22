aefd10a87b0519e39db6aaea50793548b55a2339
naufal <naufal.nurhafizh.10.01@gmail.com>
2024-03-22 08:59:23 +0700

    Add project files.

---
+++ Praktikum1/Form1.Designer.vb
@@ -0,0 +1,376 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmHitungNilai
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmHitungNilai))
+        JumlahSementara = New Label()
+        Num1 = New Button()
+        Num2 = New Button()
+        Num3 = New Button()
+        Num4 = New Button()
+        Num5 = New Button()
+        Num6 = New Button()
+        Num7 = New Button()
+        Num8 = New Button()
+        Num9 = New Button()
+        BtBagi = New Button()
+        BtKali = New Button()
+        BtKurang = New Button()
+        BtJumlah = New Button()
+        Num0 = New Button()
+        BtTambah = New Button()
+        BtDelete = New Button()
+        BtClearAll = New Button()
+        BtClear = New Button()
+        LabelTotal = New Label()
+        TxtJumlah = New TextBox()
+        NumDot = New Button()
+        SuspendLayout()
+        ' 
+        ' JumlahSementara
+        ' 
+        JumlahSementara.Enabled = False
+        JumlahSementara.FlatStyle = FlatStyle.System
+        JumlahSementara.Font = New Font("Segoe UI", 12.0F)
+        JumlahSementara.ForeColor = SystemColors.AppWorkspace
+        JumlahSementara.ImageAlign = ContentAlignment.MiddleRight
+        JumlahSementara.Location = New Point(23, 20)
+        JumlahSementara.Name = "JumlahSementara"
+        JumlahSementara.RightToLeft = RightToLeft.Yes
+        JumlahSementara.Size = New Size(354, 30)
+        JumlahSementara.TabIndex = 0
+        ' 
+        ' Num1
+        ' 
+        Num1.Font = New Font("Segoe UI", 12.0F)
+        Num1.Location = New Point(23, 436)
+        Num1.Name = "Num1"
+        Num1.Size = New Size(84, 100)
+        Num1.TabIndex = 2
+        Num1.TabStop = False
+        Num1.Tag = "1"
+        Num1.Text = "1"
+        Num1.UseVisualStyleBackColor = True
+        ' 
+        ' Num2
+        ' 
+        Num2.Font = New Font("Segoe UI", 12.0F)
+        Num2.Location = New Point(113, 436)
+        Num2.Name = "Num2"
+        Num2.Size = New Size(84, 100)
+        Num2.TabIndex = 3
+        Num2.TabStop = False
+        Num2.Tag = "2"
+        Num2.Text = "2"
+        Num2.UseVisualStyleBackColor = True
+        ' 
+        ' Num3
+        ' 
+        Num3.Font = New Font("Segoe UI", 12.0F)
+        Num3.Location = New Point(203, 436)
+        Num3.Name = "Num3"
+        Num3.Size = New Size(84, 100)
+        Num3.TabIndex = 4
+        Num3.TabStop = False
+        Num3.Tag = "3"
+        Num3.Text = "3"
+        Num3.UseVisualStyleBackColor = True
+        ' 
+        ' Num4
+        ' 
+        Num4.Font = New Font("Segoe UI", 12.0F)
+        Num4.Location = New Point(23, 330)
+        Num4.Name = "Num4"
+        Num4.Size = New Size(84, 100)
+        Num4.TabIndex = 5
+        Num4.TabStop = False
+        Num4.Tag = "4"
+        Num4.Text = "4"
+        Num4.UseVisualStyleBackColor = True
+        ' 
+        ' Num5
+        ' 
+        Num5.Font = New Font("Segoe UI", 12.0F)
+        Num5.Location = New Point(113, 330)
+        Num5.Name = "Num5"
+        Num5.Size = New Size(84, 100)
+        Num5.TabIndex = 6
+        Num5.TabStop = False
+        Num5.Tag = "5"
+        Num5.Text = "5"
+        Num5.UseVisualStyleBackColor = True
+        ' 
+        ' Num6
+        ' 
+        Num6.Font = New Font("Segoe UI", 12.0F)
+        Num6.Location = New Point(203, 330)
+        Num6.Name = "Num6"
+        Num6.Size = New Size(84, 100)
+        Num6.TabIndex = 7
+        Num6.TabStop = False
+        Num6.Tag = "6"
+        Num6.Text = "6"
+        Num6.UseVisualStyleBackColor = True
+        ' 
+        ' Num7
+        ' 
+        Num7.Font = New Font("Segoe UI", 12.0F)
+        Num7.Location = New Point(23, 224)
+        Num7.Name = "Num7"
+        Num7.Size = New Size(84, 100)
+        Num7.TabIndex = 8
+        Num7.TabStop = False
+        Num7.Tag = "7"
+        Num7.Text = "7"
+        Num7.UseVisualStyleBackColor = True
+        ' 
+        ' Num8
+        ' 
+        Num8.Font = New Font("Segoe UI", 12.0F)
+        Num8.Location = New Point(113, 224)
+        Num8.Name = "Num8"
+        Num8.Size = New Size(84, 100)
+        Num8.TabIndex = 9
+        Num8.TabStop = False
+        Num8.Tag = "8"
+        Num8.Text = "8"
+        Num8.UseVisualStyleBackColor = True
+        ' 
+        ' Num9
+        ' 
+        Num9.Font = New Font("Segoe UI", 12.0F)
+        Num9.Location = New Point(203, 224)
+        Num9.Name = "Num9"
+        Num9.Size = New Size(84, 100)
+        Num9.TabIndex = 10
+        Num9.TabStop = False
+        Num9.Tag = "9"
+        Num9.Text = "9"
+        Num9.UseVisualStyleBackColor = True
+        ' 
+        ' BtBagi
+        ' 
+        BtBagi.Font = New Font("Segoe UI", 12.0F)
+        BtBagi.Location = New Point(293, 118)
+        BtBagi.Name = "BtBagi"
+        BtBagi.Size = New Size(84, 68)
+        BtBagi.TabIndex = 14
+        BtBagi.TabStop = False
+        BtBagi.Tag = "/"
+        BtBagi.Text = "/"
+        BtBagi.UseVisualStyleBackColor = True
+        ' 
+        ' BtKali
+        ' 
+        BtKali.Font = New Font("Segoe UI", 12.0F)
+        BtKali.Location = New Point(293, 192)
+        BtKali.Name = "BtKali"
+        BtKali.Size = New Size(84, 68)
+        BtKali.TabIndex = 15
+        BtKali.TabStop = False
+        BtKali.Tag = "*"
+        BtKali.Text = "x"
+        BtKali.UseVisualStyleBackColor = True
+        ' 
+        ' BtKurang
+        ' 
+        BtKurang.Font = New Font("Segoe UI", 12.0F)
+        BtKurang.Location = New Point(293, 266)
+        BtKurang.Name = "BtKurang"
+        BtKurang.Size = New Size(84, 68)
+        BtKurang.TabIndex = 16
+        BtKurang.TabStop = False
+        BtKurang.Tag = "-"
+        BtKurang.Text = "-"
+        BtKurang.UseVisualStyleBackColor = True
+        ' 
+        ' BtJumlah
+        ' 
+        BtJumlah.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold)
+        BtJumlah.Location = New Point(293, 414)
+        BtJumlah.Name = "BtJumlah"
+        BtJumlah.Size = New Size(84, 212)
+        BtJumlah.TabIndex = 17
+        BtJumlah.TabStop = False
+        BtJumlah.Tag = True
+        BtJumlah.Text = "="
+        BtJumlah.UseVisualStyleBackColor = True
+        ' 
+        ' Num0
+        ' 
+        Num0.CausesValidation = False
+        Num0.Font = New Font("Segoe UI", 12.0F)
+        Num0.Location = New Point(23, 542)
+        Num0.Name = "Num0"
+        Num0.Size = New Size(174, 84)
+        Num0.TabIndex = 18
+        Num0.TabStop = False
+        Num0.Tag = "0"
+        Num0.Text = "0"
+        Num0.UseVisualStyleBackColor = True
+        ' 
+        ' BtTambah
+        ' 
+        BtTambah.Font = New Font("Segoe UI", 12.0F)
+        BtTambah.Location = New Point(293, 340)
+        BtTambah.Name = "BtTambah"
+        BtTambah.Size = New Size(84, 68)
+        BtTambah.TabIndex = 19
+        BtTambah.TabStop = False
+        BtTambah.Tag = "+"
+        BtTambah.Text = "+"
+        BtTambah.UseVisualStyleBackColor = True
+        ' 
+        ' BtDelete
+        ' 
+        BtDelete.Image = CType(resources.GetObject("BtDelete.Image"), Image)
+        BtDelete.Location = New Point(203, 118)
+        BtDelete.Name = "BtDelete"
+        BtDelete.Size = New Size(84, 100)
+        BtDelete.TabIndex = 13
+        BtDelete.TabStop = False
+        BtDelete.UseVisualStyleBackColor = True
+        ' 
+        ' BtClearAll
+        ' 
+        BtClearAll.Location = New Point(23, 118)
+        BtClearAll.Name = "BtClearAll"
+        BtClearAll.Size = New Size(84, 100)
+        BtClearAll.TabIndex = 11
+        BtClearAll.TabStop = False
+        BtClearAll.Text = "AC"
+        BtClearAll.UseVisualStyleBackColor = True
+        ' 
+        ' BtClear
+        ' 
+        BtClear.Location = New Point(113, 118)
+        BtClear.Name = "BtClear"
+        BtClear.Size = New Size(84, 100)
+        BtClear.TabIndex = 12
+        BtClear.TabStop = False
+        BtClear.Text = "C"
+        BtClear.UseVisualStyleBackColor = True
+        ' 
+        ' LabelTotal
+        ' 
+        LabelTotal.AutoSize = True
+        LabelTotal.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold)
+        LabelTotal.ForeColor = SystemColors.ActiveBorder
+        LabelTotal.Location = New Point(23, 60)
+        LabelTotal.Name = "LabelTotal"
+        LabelTotal.Size = New Size(59, 28)
+        LabelTotal.TabIndex = 20
+        LabelTotal.Text = "Total"
+        ' 
+        ' TxtJumlah
+        ' 
+        TxtJumlah.BackColor = SystemColors.Control
+        TxtJumlah.BorderStyle = BorderStyle.None
+        TxtJumlah.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TxtJumlah.ForeColor = SystemColors.WindowText
+        TxtJumlah.Location = New Point(83, 61)
+        TxtJumlah.MaxLength = 24
+        TxtJumlah.Name = "TxtJumlah"
+        TxtJumlah.RightToLeft = RightToLeft.No
+        TxtJumlah.ShortcutsEnabled = False
+        TxtJumlah.Size = New Size(294, 27)
+        TxtJumlah.TabIndex = 21
+        TxtJumlah.TabStop = False
+        TxtJumlah.Text = "0"
+        TxtJumlah.TextAlign = HorizontalAlignment.Right
+        TxtJumlah.WordWrap = False
+        ' 
+        ' NumDot
+        ' 
+        NumDot.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        NumDot.Location = New Point(203, 542)
+        NumDot.Name = "NumDot"
+        NumDot.Size = New Size(84, 84)
+        NumDot.TabIndex = 22
+        NumDot.Tag = "."
+        NumDot.Text = "."
+        NumDot.UseVisualStyleBackColor = True
+        ' 
+        ' FrmHitungNilai
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonFace
+        ClientSize = New Size(402, 653)
+        Controls.Add(NumDot)
+        Controls.Add(TxtJumlah)
+        Controls.Add(LabelTotal)
+        Controls.Add(BtTambah)
+        Controls.Add(Num0)
+        Controls.Add(BtJumlah)
+        Controls.Add(BtKurang)
+        Controls.Add(BtKali)
+        Controls.Add(BtBagi)
+        Controls.Add(BtDelete)
+        Controls.Add(BtClear)
+        Controls.Add(BtClearAll)
+        Controls.Add(Num9)
+        Controls.Add(Num8)
+        Controls.Add(Num7)
+        Controls.Add(Num6)
+        Controls.Add(Num5)
+        Controls.Add(Num4)
+        Controls.Add(Num3)
+        Controls.Add(Num2)
+        Controls.Add(Num1)
+        Controls.Add(JumlahSementara)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "FrmHitungNilai"
+        ShowIcon = False
+        SizeGripStyle = SizeGripStyle.Hide
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Kalkulator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents JumlahSementara As Label
+    Friend WithEvents Num1 As Button
+    Friend WithEvents Num2 As Button
+    Friend WithEvents Num3 As Button
+    Friend WithEvents Num4 As Button
+    Friend WithEvents Num5 As Button
+    Friend WithEvents Num6 As Button
+    Friend WithEvents Num7 As Button
+    Friend WithEvents Num8 As Button
+    Friend WithEvents Num9 As Button
+    Friend WithEvents BtBagi As Button
+    Friend WithEvents BtKali As Button
+    Friend WithEvents BtKurang As Button
+    Friend WithEvents BtJumlah As Button
+    Friend WithEvents Num0 As Button
+    Friend WithEvents BtTambah As Button
+    Friend WithEvents BtDelete As Button
+    Friend WithEvents BtClearAll As Button
+    Friend WithEvents BtClear As Button
+    Friend WithEvents LabelTotal As Label
+    Friend WithEvents TxtJumlah As TextBox
+    Friend WithEvents NumDot As Button
+
+End Class
---
+++ Praktikum1/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.Globalization
+
+Public Class FrmHitungNilai
+    Protected Attribute As Char = Nothing
+
+    Private Sub KeyboardValidation(sender As Object, e As KeyPressEventArgs) Handles TxtJumlah.KeyPress
+        e.Handled = True
+    End Sub
+
+    Private Sub InputNum(sender As Object, e As MouseEventArgs) Handles NumDot.MouseClick, Num0.MouseClick, Num1.MouseClick, Num2.MouseClick, Num3.MouseClick, Num4.MouseClick, Num5.MouseClick, Num6.MouseClick, Num7.MouseClick, Num8.MouseClick, Num9.MouseClick
+        Dim clickedNum As String = DirectCast(sender, Control).Tag
+        Dim digit As Char = clickedNum
+
+        If TxtJumlah.Text = "0" AndAlso digit <> "." Then
+            TxtJumlah.Text = digit
+        ElseIf TxtJumlah.TextLength < 12 Then
+            TxtJumlah.AppendText(digit)
+        End If
+    End Sub
+
+    Private Sub DeleteNum(sender As Object, e As MouseEventArgs) Handles BtDelete.MouseClick
+        If TxtJumlah.TextLength > 0 Then
+            TxtJumlah.Text = TxtJumlah.Text.Remove(TxtJumlah.TextLength - 1, 1)
+        End If
+    End Sub
+
+    Private Sub ClearNum(sender As Object, e As MouseEventArgs) Handles BtClear.MouseClick
+        ClearNum()
+    End Sub
+
+    Private Sub ClearNum()
+        TxtJumlah.Clear()
+    End Sub
+
+    Private Sub SetDefaultZero(sender As Object, e As EventArgs) Handles TxtJumlah.TextChanged
+        If TxtJumlah.TextLength = 0 Then
+            TxtJumlah.Text = "0"
+        End If
+    End Sub
+
+    Private Function SetValueTotalSementara(values() As Double) As String
+        Dim result As String = ""
+
+        If values.Length > 1 AndAlso Not String.IsNullOrEmpty(GetAttribute().ToString) Then
+            result = values(0)
+
+            For i As Integer = 1 To values.Length - 1
+                result &= $" {GetAttribute()} {values(i)}"
+            Next
+        End If
+
+        Return result
+    End Function
+
+    Private Function CalculateTotal(values() As Double) As Double
+        Dim total As Double = values(0)
+
+        If values.Length > 1 Then
+            For i As Integer = 1 To values.Length - 1
+                Dim item As Double = values(i)
+                Select Case GetAttribute().ToString()
+                    Case "+"
+                        total += item
+                    Case "-"
+                        total = item - total
+                    Case "*"
+                        total *= item
+                    Case "/"
+                        If item <> 0 Then
+                            total = item / total
+                        End If
+                End Select
+            Next
+        End If
+
+        Return total
+    End Function
+
+    Private Sub SetAttribute(Attribute As Char)
+        Me.Attribute = Attribute
+    End Sub
+
+    Private Function GetAttribute() As Char
+        Return Attribute
+    End Function
+
+    Private Sub OperatorButtonClick(sender As Object, e As MouseEventArgs) Handles BtTambah.MouseClick, BtKurang.MouseClick, BtKali.MouseClick, BtBagi.MouseClick
+        Dim operatorButton As Button = DirectCast(sender, Button)
+        SetAttribute(operatorButton.Tag()(0))
+        JumlahSementara.Text = TxtJumlah.Text
+        ClearNum()
+    End Sub
+
+    Private Sub CalculateResult(sender As Object, e As MouseEventArgs) Handles BtJumlah.MouseClick
+        Dim values() As Double
+        Dim result As String = "0"
+
+        If Not String.IsNullOrEmpty(GetAttribute().ToString) AndAlso Not String.IsNullOrEmpty(JumlahSementara.Text) AndAlso IsNumeric(JumlahSementara.Text) Then
+            values = {CDbl(Val(TxtJumlah.Text)), CDbl(Val(JumlahSementara.Text))}
+            result = CalculateTotal(values)
+            JumlahSementara.Text = SetValueTotalSementara(values)
+        End If
+
+        TxtJumlah.Text = result
+    End Sub
+
+    Private Sub ClearAll(sender As Object, e As MouseEventArgs) Handles BtClearAll.MouseClick
+        ClearNum()
+        JumlahSementara.Text = ""
+    End Sub
+End Class
